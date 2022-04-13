33c63ade266cd0f9036fde584f4f6401650040f5
Unknown <mdymanalo@gmail.com>
2022-04-13 14:12:11 +0900

    Fixed log error on backward

--- loss_factory.py
+++ loss_factory.py
@@ -6,17 +6,24 @@ class Loglikelihood(torch.nn.Module):
         self.sigmoid = torch.nn.Sigmoid()
  
     def forward(self, pred, prop, labels):        
-        pred = self.sigmoid(pred)
-        prop = self.sigmoid(prop)
+        pred = torch.nan_to_num(self.sigmoid(pred))
+        prop = torch.nan_to_num(self.sigmoid(prop))
 
         prob_labeled = pred*prop
         prob_unlabeled_pos = pred*(1-prop)
         prob_unlabeled_neg = 1-pred
         div = prob_unlabeled_pos+prob_unlabeled_neg
+        div[div == 0] = 0.00000001
 
         prob_pos_given_unl = prob_unlabeled_pos/div
+        # prob_pos_given_unl = torch.nan_to_num(prob_unlabeled_pos/div)
         prob_neg_given_unl = 1-prob_pos_given_unl
         
+        #prevent problems of taking log
+        prob_labeled = prob_labeled + 0.00000001
+        prob_unlabeled_pos = prob_unlabeled_pos + 0.00000001
+        prob_unlabeled_neg = prob_unlabeled_neg + 0.00000001
+
         ll = -(
             labels*torch.log(prob_labeled)+
             (1-labels)*(
